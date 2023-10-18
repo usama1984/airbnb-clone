bf306cea4dd4bb22b5a89dbe939cf7d6ddfc4645
usama <usamamuhammad231@gmail.com>
2023-10-18 22:39:47 +0500

    header complete

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Home from './Home/Home'
+import Header from './Header/Header'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Header />
+
+      <Home />
+
+      {/* home */}
+          {/* header */}
+          {/* banner */}
+          {/* cards */}
+          {/* footer */}
+      
+      {/* search page */}
+    </div>
+  )
+}
+
+export default App
