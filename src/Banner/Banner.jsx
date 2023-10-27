a3ab6be0b0d755d2a351710e8663f84fb2f73d6c
usama <usamamuhammad231@gmail.com>
2023-10-27 23:51:46 +0500

    search page created and router added

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,8 +2,10 @@ import React, {useState} from 'react'
 import './Banner.css'
 import { Button } from '@mui/material';
 import Search from '../search/Search';
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+  const navigate = useNavigate();
   const [showSearch, setShowSearch] = useState(false);
   return (
     <div className='banner'>
@@ -16,7 +18,7 @@ const Banner = () => {
         <div className='banner__info'>
           <h1>Get out and stretch your imagination</h1>
           <h5>Plan a different kind of gateway to uncover the hidden gems near you.</h5>
-          <Button variant='outlined'>Explore Nearby</Button>
+          <Button onClick={() => navigate('/search')} variant='outlined'>Explore Nearby</Button>
         </div>
     </div>
   )
--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -5,8 +5,11 @@ import { DateRangePicker } from "react-date-range";
 import './Search.css'
 import { Button } from '@mui/material';
 import PeopleIcon from '@mui/icons-material/People';
+import { useNavigate } from "react-router-dom";
+
 
 const Search = () => {
+  const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -25,7 +28,7 @@ const Search = () => {
       <DateRangePicker ranges={[selectionRange]} onChange={handleSelect}/>
       <h2>Number of guests <PeopleIcon /> </h2>
       <input min={0} defaultValue={2} type='number'/>
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => navigate('/search')}>Search Airbnb</Button>
     </div>
   )
 }
